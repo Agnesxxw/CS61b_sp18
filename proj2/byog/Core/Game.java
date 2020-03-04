b48d43a041477fc5d588e8408ac5d62a6a709613
Agnesxxw <Agnes_xxw@outlook.com>
2020-03-04 14:41:48 +0800

    complete worldgenerator

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -1,16 +1,39 @@
+/**
+ * @author: Agnes Xu
+ * @date: 2020/3/4
+ * @version: 1.0
+ */
+
 package byog.Core;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import java.awt.Font;
 
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
-import java.awt.Font;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Game {
-    TERenderer ter = new TERenderer();
-    /* Feel free to change the width and height. */
+
+    /// static final variables
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
+    private static final int ENTRYX = 40;
+    private static final int ENTRYY = 5;
+    private static final String NORTH = "w";
+    private static final String EAST = "d";
+    private static final String SOUTH = "s";
+    private static final String WEST = "a";
+    private static final String PATH = "saved.txt";
     public static final int welcomeBoardX = 300;
     public static final int welcomeBoardY = 400;
     Font font1 = new Font("Times New Roman", Font.BOLD, 50);
@@ -25,6 +48,89 @@ public class Game {
     private TETile[][] world;
     private int playerX;
     private int playerY;
+
+    private void switchSetupMode() {
+        setupMode = !setupMode;
+    }
+
+    private void switchNewGameMode() {
+        newGameMode = !newGameMode;
+    }
+
+    private void switchQuitMode() {
+        quitMode = !quitMode;
+    }
+
+    /* Processes game recursively according to a given input Strings */
+    private void processInput(String input) {
+
+        if (input == null) {
+            System.out.println("No input given.");
+            System.exit(0);
+        }
+
+        String first = Character.toString(input.charAt(0));
+        first = first.toLowerCase(); // normalize an input to lower case
+        processInputString(first);
+
+        if (input.length() > 1) {
+            String rest = input.substring(1);
+            processInput(rest); // recursive call until input ends
+        }
+
+    }
+    /* Processes game according to a given single input String */
+    private void processInputString(String first) {
+
+        if (setupMode) {      // when the setup hasn't been done
+            switch (first) {
+                case "n":       // new game gonna be generated
+                    switchNewGameMode();
+                    break;
+                case "s":       // setup a new game
+                    setupNewGame();
+                    break;
+                case "l":       // load the previously saved game
+                    load();
+                    break;
+                case "q":
+                    System.exit(0);
+                    break;
+                default:        // append next seed integer to seedString
+                    try {
+                        Long.parseLong(first);
+                        seedString += first;
+                    } catch (NumberFormatException e) { // exit program if input is invalid
+                        System.out.println("Invalid input given: " + first);
+                        System.exit(0);
+                    }
+                    break;
+            }
+        } else {                // when the setup has been done
+            switch (first) {
+                // @Note: Add my keyboard preferences
+                case NORTH:
+                case "o":
+                case EAST:
+                case "l":
+                case SOUTH:
+                case "n":
+                case WEST:
+                case "k":
+                    move(first);
+                    break;
+                case ":":
+                    switchQuitMode();
+                    break;
+                case "q":
+                    saveAndQuit();
+                    System.exit(0);
+                    break;
+                default:
+            }
+        }
+
+    }
 
     /**
      * Display welcome msg
@@ -74,7 +180,6 @@ public class Game {
         // drawn if the same inputs had been given to playWithKeyboard().
 
         TETile[][] finalWorldFrame = null;
-        Dra
         return finalWorldFrame;
     }
 
--- proj2/byog/Core/worldGenerator.java
+++ proj2/byog/Core/worldGenerator.java
@@ -1,10 +1,19 @@
-package byog.Core;
+/**
+ * @author: Agnes Xu
+ * @date: 2020/3/3
+ * @version: 1.0
+ */
 
+package byog.Core;
+/**
+ * Random world generator
+ */
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
-import byog.lab5.HexWorld;
 import java.util.Random;
+import java.util.List;
+import java.util.LinkedList;
 
 public class worldGenerator {
 
@@ -143,11 +152,29 @@ public class worldGenerator {
     }
 
     /**
+     * return new direction if this direction isn't available
+     * @param direction
+     * @return new direction
+     */
+    private String getReverseDirc(String direction){
+        switch (direction){
+            case NORTH:
+                return SOUTH;
+            case SOUTH:
+                return NORTH;
+            case WEST:
+                return EAST;
+            default:
+                return WEST;
+        }
+    }
+
+    /**
      * return the random new room position[leftbottom, righttop] coordinate in the north
      * @param point start point
      * @param W width of the room
      * @param H height of the room
-     * @return
+     * @return the new coordination of northroom
      */
     private Position[] newNorthPosition(Position point, int W, int H){
         int px = point.x;
@@ -171,7 +198,7 @@ public class worldGenerator {
      * @param point start point
      * @param W width of the room
      * @param H height of the room
-     * @return
+     * @return the new coordination of eastroom
      */
     private Position[] newEastPosition(Position point, int W, int H) {
         int px = point.x;
@@ -195,7 +222,7 @@ public class worldGenerator {
      * @param point start point
      * @param W width of the room
      * @param H height of the room
-     * @return
+     * @return the new coordination of westroom
      */
     private Position[] newWestPosition(Position point, int W, int H) {
         int px = point.x;
@@ -219,7 +246,7 @@ public class worldGenerator {
      * @param point start point
      * @param W width of the room
      * @param H height of the room
-     * @return
+     * @return the new coordination of southroom
      */
     private Position[] newSouthPosition(Position point, int W, int H) {
         int px = point.x;
@@ -238,4 +265,194 @@ public class worldGenerator {
         }
     }
 
+    /**
+     * Returns random exit position and direction from a given rectangular room
+     * @param leftBottom current room leftbottom coordinate
+     * @param rightTop current room righttop coordinate
+     * @param currentDirection current direction
+     * @return new direction and new position coordinate after move towards the new random direction
+     */
+    private Object[] randomExit(Position leftBottom, Position rightTop, String currentDirection) {
+        int lbX = leftBottom.x;
+        int lbY = leftBottom.y;
+        int rtX = rightTop.x;
+        int rtY = rightTop.y;
+        int w = rtX - lbX - 1;
+        int h = rtY - lbY - 1;
+        Object[] exitAndDirection = new Object[2];
+
+        // Decide nextDirection using Linkedlist data structuere
+        List<String> directions = new LinkedList<>();
+        directions.add(NORTH);
+        directions.add(EAST);
+        directions.add(SOUTH);
+        directions.add(WEST);
+        directions.remove(getReverseDirc(currentDirection)); // prevent back to previous direction
+        // eg : from north to south -> current direction = south, so remove north direction, otherwise, back to original
+        String nextDirection = directions.get(random.nextInt(directions.size())); // then generate a random direction
+
+        // Decide next exitPosition
+        Position nextExitPosition;
+        switch (nextDirection) {
+            case NORTH:
+                nextExitPosition = new Position(rtX - random.nextInt(w) - 1, rtY);
+                break;
+            case EAST:
+                nextExitPosition = new Position(rtX, rtY - random.nextInt(h) - 1);
+                break;
+            case SOUTH:
+                nextExitPosition = new Position(lbX + random.nextInt(w) + 1, lbY);
+                break;
+            default:
+                nextExitPosition = new Position(lbX, lbY + random.nextInt(h) + 1);
+                break;
+        }
+
+        exitAndDirection[0] = nextExitPosition;
+        exitAndDirection[1] = nextDirection;
+        return exitAndDirection;
+    }
+    // @aviatesk
+    /* Makes a mono exit from a given rectangular room and calls another recursiveAddRandomRoom */
+    private void monoExit(Position leftBottom, Position rightTop, String currentDirection) {
+        Object[] exitAndDirection = randomExit(leftBottom, rightTop, currentDirection);
+        Position nextExitPosition = (Position) exitAndDirection[0];
+        String nextDirection = (String) exitAndDirection[1];
+        recursiveAddRandomRoom(nextExitPosition, nextDirection);
+    }
+
+    // @aviatesk
+    /* Makes bi exits from a given rectangular room and calls another recursiveAddRandomRoom */
+    private void biExit(Position leftBottom, Position rightUpper, String currentDirection) {
+        Object[] exitAndDirection1 = randomExit(leftBottom, rightUpper, currentDirection);
+        Position nextExitPosition1 = (Position) exitAndDirection1[0];
+        String nextDirection1 = (String) exitAndDirection1[1];
+
+        Object[] exitAndDirection2;
+        Position nextExitPosition2 = nextExitPosition1;
+        String nextDirection2 = nextDirection1;
+        while (nextDirection2.equals(nextDirection1)) {
+            exitAndDirection2 = randomExit(leftBottom, rightUpper, currentDirection);
+            nextExitPosition2 = (Position) exitAndDirection2[0];
+            nextDirection2 = (String) exitAndDirection2[1];
+        }
+
+        recursiveAddRandomRoom(nextExitPosition1, nextDirection1);
+        recursiveAddRandomRoom(nextExitPosition2, nextDirection2);
+    }
+    // @aviatesk
+    /* Makes tri exits from a given rectangular room and calls another recursiveAddRandomRoom */
+    private void triExit(Position leftBottom, Position rightUpper, String currentDirection) {
+        Object[] exitAndDirection1 = randomExit(leftBottom, rightUpper, currentDirection);
+        Position nextExitPosition1 = (Position) exitAndDirection1[0];
+        String nextDirection1 = (String) exitAndDirection1[1];
+
+        Object[] exitAndDirection2;
+        Position nextExitPosition2 = nextExitPosition1;
+        String nextDirection2 = nextDirection1;
+        while (nextDirection2.equals(nextDirection1)) {
+            exitAndDirection2 = randomExit(leftBottom, rightUpper, currentDirection);
+            nextExitPosition2 = (Position) exitAndDirection2[0];
+            nextDirection2 = (String) exitAndDirection2[1];
+        }
+
+        Object[] exitAndDirection3;
+        Position nextExitPosition3 = nextExitPosition1;
+        String nextDirection3 = nextDirection1;
+        while (nextDirection3.equals(nextDirection1) || nextDirection3.equals(nextDirection2)) {
+            exitAndDirection3 = randomExit(leftBottom, rightUpper, currentDirection);
+            nextExitPosition3 = (Position) exitAndDirection3[0];
+            nextDirection3 = (String) exitAndDirection3[1];
+        }
+
+        recursiveAddRandomRoom(nextExitPosition1, nextDirection1);
+        recursiveAddRandomRoom(nextExitPosition2, nextDirection2);
+        recursiveAddRandomRoom(nextExitPosition3, nextDirection3);
+    }
+
+    // @aviatesk
+    /* Recursively adds random rooms */
+    private void recursiveAddRandomRoom(Position exitPosition, String currentDirection) {
+
+        int exitX = exitPosition.x;
+        int exitY = exitPosition.y;
+        int w = random.nextInt(MAXROOMWIDTH) + 1;
+        int h = random.nextInt(MAXROOMHEIGHT) + 1;
+
+        Position entryPosition;
+        Position[] lrPositions;
+        switch (currentDirection) {
+            case NORTH:
+                entryPosition = new Position(exitX, exitY + 1);
+                lrPositions = newNorthPosition(entryPosition, w, h);
+                break;
+            case EAST:
+                entryPosition = new Position(exitX + 1, exitY);
+                lrPositions = newEastPosition(entryPosition, w, h);
+                break;
+            case SOUTH:
+                entryPosition = new Position(exitX, exitY - 1);
+                lrPositions = newSouthPosition(entryPosition, w, h);
+                break;
+            default:
+                entryPosition = new Position(exitX - 1, exitY);
+                lrPositions = newWestPosition(entryPosition, w, h);
+                break;
+        }
+
+        if (lrPositions != null) {
+
+            Exit(exitPosition);
+            Position leftBottom = lrPositions[0];
+            Position rightUpper = lrPositions[1];
+            generateRoom(leftBottom, rightUpper);
+            entrance(entryPosition);
+
+            switch (random.nextInt(3) + 1) {
+                /* Comment in below for more simpler world */
+//                case 1: monoExit(leftBottom, rightUpper, currentDirection); break;
+                case 2:
+                    biExit(leftBottom, rightUpper, currentDirection);
+                    break;
+                default:
+                    triExit(leftBottom, rightUpper, currentDirection);
+                    break;
+            }
+
+        }
+
+    }
+
+    /**
+     * Returns a world generated with given random seed
+     *
+     * @return a randomly generated world
+     */
+    TETile[][] generate() {
+        initialize();
+
+        // Make the first room
+        Position[] lrPositions = newNorthPosition(initialPosition,MAXROOMWIDTH, MAXROOMHEIGHT);
+        Position leftBottom = lrPositions[0];
+        Position rightTop = lrPositions[1];
+        generateRoom(leftBottom, rightTop);
+        generateEntrance(initialPosition);
+
+        // Recursively call recursiveAddRandomRoom via first calling triExit
+        triExit(leftBottom, rightTop, NORTH);
+
+        return world;
+    }
+
+    // Main method just to check this class works itself
+    public static void main(String[] args) {
+        int w = 80;
+        int h = 50;
+        TERenderer ter = new TERenderer();
+        ter.initialize(w, h);
+        worldGenerator wg = new worldGenerator(w, h, 40, 5, 42);
+        wg.initialize();
+        wg.generate();
+        ter.renderFrame(wg.world);
+    }
 }
